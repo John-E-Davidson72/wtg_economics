a55120a692cf00b127f4d314bc74a9134f1a92f7
John Davidson <john.davidson.ctr@hotmail.co.uk>
2023-05-20 22:23:52 +0100

    0.0

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@ document.write(" <footer>\
 <div class=\"row\">\
   <div class=\"col-lg-12\">\
     <p class=\"text-center\">Built by <a href=\"mailto:john.davidson.ctr@hotmail.co.uk\">John Davidson</a> |\
-    Distributed under the <a href=\"#\">MIT License</a> |\
+    Distributed under the <a href=\"https://github.com/John-E-Davidson72/wtg_economics/blob/master/LICENSE.md\">MIT License</a> |\
     Built with <a href=\"https://getbootstrap.com/\" rel=\"nofollow\">Bootstrap</a> |\
     Turbines icon from <a href=\"https://www.flaticon.com/free-icons/wind-farm\" rel=\"nofollow\">Flaticon</a></p>\
   </div>\
